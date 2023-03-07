91b02572a886145b301989c2e1b818a8540c2112
nbransby <nbransby@gmail.com>
2023-03-07 20:53:34 +0000

    bunp firebase-java

--- build.gradle.kts
+++ build.gradle.kts
@@ -208,7 +208,7 @@ subprojects {
         }
 
         dependencies {
-            "jvmMainApi"("dev.gitlive:firebase-java-sdk:1.0.15")
+            "jvmMainApi"("dev.gitlive:firebase-java-sdk:1.0.16")
             "jvmMainApi"("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.0") {
                 exclude("com.google.android.gms")
             }
